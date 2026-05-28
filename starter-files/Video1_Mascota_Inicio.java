211d21ca37e5b7c788a0dceea277bf4d2a799368
Victor Manuel Alberca Saavedra <104582047+Agnizzz@users.noreply.github.com>
2026-05-27 22:01:51 -0500

    Create base structure for Mascota and Main classes
    Initial implementation of Mascota class and Main class.

---
+++ starter-files/Video1_Mascota_Inicio.java
@@ -0,0 +1,18 @@
+// Este es el archivo base para la Lección 7 (Clases y Objetos)
+class Mascota {
+    // Aquí irán las características de nuestra mascota
+
+    
+    // Aquí irá el Constructor
+    
+
+    // Aquí irá el Método (lo que puede hacer)
+    
+}
+
+public class Main {
+    public static void main(String[] args) {
+        // Aquí crearemos nuestro primer objeto mascota
+        
+    }
+}
