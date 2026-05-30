63521a9b759840dca5eedb4ff0e8ae36404a6449
blafyy <145238559+blafyy@users.noreply.github.com>
2026-05-30 14:21:38 -0500

    Add files via upload

---
+++ starter-files/Leccion1_Inicio.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Mostrar un mensaje en pantalla
+
+        // Variables de tipo texto (String)
+
+        // Variables de tipo número entero (int)
+
+        // Variables de tipo número decimal (double)
+
+        // Variables de tipo verdadero o falso (boolean)
+
+        // Mostrar las variables en pantalla
+
+    }
+}
